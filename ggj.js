c746fd22f92060a54461ec9899efe6d9f3324e63
Sheva Goldberg <sheva@jellyvision.com>
2016-01-30 13:57:22 -0600

    messing with scales

--- ggj.js
+++ ggj.js
@@ -1,5 +1,16 @@
-var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
+//c scale: 440, 493, 554, 587, 659, 739, 830, 880
+var dict = {0:440, 1:493, 2:554, 3:587};
 
+dict = T("ndict", {dict:dict});
+T("tri", {freq:dict, mul:0.25}).play();
+
+
+T("interval", {interval:500}, function(count) {
+    dict.index = count % 4;
+}).start();
+
+/**
+var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
 var keydict = T("ndict.key");
 var midicps = T("midicps");
 T("keyboard").on("keydown", function(e) {
@@ -15,3 +26,4 @@ T("keyboard").on("keydown", function(e) {
     synth.noteOff(midi, 100);
   }
 }).start();
+**/
