db60b43af304d4f58d6387c058bc8e8a92a3642d
Sheva Goldberg <sheva@jellyvision.com>
2016-01-30 13:39:26 -0600

    add keyboard.js and example

--- ggj.js
+++ ggj.js
@@ -1,1 +1,17 @@
-T("sin", {freq:440, mul:0.5}).play();
+var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
+
+var keydict = T("ndict.key");
+var midicps = T("midicps");
+T("keyboard").on("keydown", function(e) {
+  var midi = keydict.at(e.keyCode);
+  console.log(midi);
+  if (midi) {
+    var freq = midicps.at(midi);
+    synth.noteOnWithFreq(freq, 100);
+  }
+}).on("keyup", function(e) {
+  var midi = keydict.at(e.keyCode);
+  if (midi) {
+    synth.noteOff(midi, 100);
+  }
+}).start();
